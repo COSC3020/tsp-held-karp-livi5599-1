3a51ddbe122074b38c316033e89c5d7aebfb9a41
livi5599 <obrown1@uwyo.edu>
2025-05-11 19:36:24 -0600

    Update code.js

--- code.js
+++ code.js
@@ -16,44 +16,63 @@ function tsp_hk(distance_matrix) {
 }
 
 function heldKarp(distance_matrix, cities, start, cache) {
+    console.log("Entered heldKarp with cities:", cities, "and start:", start);
     if (Math.abs(cities.size) == 2) { //base case
         console.log("2 cities in set");
-        for (let city of cities) {
-            console.log("city = ", city);
-            if (city != start) {
-                console.log("returning ", distance_matrix[start][city]);
-                return distance_matrix[start][city];
-            }
-        }
+        console.log("start = ", start);
+        console.log("is start in cities? ", cities.has(start));
+        let otherCity = [...cities].find(city => city !== start);
+        console.log("otherCity = ", otherCity);
+        return distance_matrix[start][otherCity];
+        // for (let city of cities) {
+        //     console.log("city = ", city);
+        //     if (city != start) {
+        //         console.log("returning ", distance_matrix[start][city]);
+        //         return distance_matrix[start][city];
+        //     }
+        // }
     }
     
     let minDist = Infinity;
     for (let city of cities) {
         console.log("city = ", city);
-        if (city == start) {
+        if (city === start) {
             continue;
         }
         let newCities = new Set(cities);
         console.log("newCities = ", newCities);
         newCities.delete(start);
         console.log("removed start from cities. newCities = ", newCities);
-        let key = JSON.stringify([...newCities].sort()) + "," + city;
+        let key = JSON.stringify([...newCities].sort()) + "|" + city;
         console.log("key = ", key);
-        let totalDistance = 0;
+        //let totalDistance = 0;
         if (cache.has(key)) {
-            totalDistance = cache.get(key) + distance_matrix[start][city];
+            minDist = Math.min(minDist, cache.get(key) + distance_matrix[start][city]);
+            continue;
+            // console.log("Cache has key, retrieving from cache:", key);
+            // totalDistance = cache.get(key) + distance_matrix[start][city];
         }
-        else {
-            console.log("distance_matrix[start][city] = ", distance_matrix[start][city]);
-            let subProb = heldKarp(distance_matrix, newCities, city, cache);
-            totalDistance = subProb + distance_matrix[start][city];
-            console.log("totalDistance = ", totalDistance);
-            cache.set(key, subProb);
-        }
-        if (totalDistance < minDist) {
-            minDist = totalDistance;
-            console.log("new minDist = ", minDist);
-        }
+
+        console.log("Cache does not have key, calculating subproblem for:", key);
+        //console.log("distance_matrix[start][city] = ", distance_matrix[start][city]);
+        let subProb = heldKarp(distance_matrix, newCities, city, cache);
+        let totalDistance = subProb + distance_matrix[start][city];
+        console.log("SubProb distance:", subProb, "Total distance:", totalDistance);
+        cache.set(key, subProb);
+        //else {
+            // console.log("Cache does not have key, calculating subproblem for:", key);
+            // //console.log("distance_matrix[start][city] = ", distance_matrix[start][city]);
+            // let subProb = heldKarp(distance_matrix, newCities, city, cache);
+            // totalDistance = subProb + distance_matrix[start][city];
+            // console.log("SubProb distance:", subProb, "Total distance:", totalDistance);
+            // cache.set(key, subProb);
+        //}
+        minDist = Math.min(minDist, totalDistance);
+        // if (totalDistance < minDist) {
+        //     minDist = totalDistance;
+        //     console.log("new minDist = ", minDist);
+        // }
     }
+    console.log("Returning minDist:", minDist);
     return minDist;
 }
